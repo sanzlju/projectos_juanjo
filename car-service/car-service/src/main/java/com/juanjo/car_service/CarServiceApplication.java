f4b3f909196c12f408ca474ca04be10ffc869569
sanzlju <jsanzleon@yahoo.com>
2025-10-12 21:47:33 +0200

    Update CarServiceApplication.java
    trying to fix register into eureka server

--- car-service/car-service/src/main/java/com/juanjo/car_service/CarServiceApplication.java
+++ car-service/car-service/src/main/java/com/juanjo/car_service/CarServiceApplication.java
@@ -2,10 +2,12 @@ package com.juanjo.car_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient	
+@EnableDiscoveryClient
+@RestController
 public class CarServiceApplication {
 
 	public static void main(String[] args) {
