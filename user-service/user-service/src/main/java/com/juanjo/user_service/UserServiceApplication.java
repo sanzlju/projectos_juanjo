8f05516e8880ff09124e03d36856d243e261aec7
sanzlju <jsanzleon@yahoo.com>
2025-10-12 21:45:32 +0200

    Update UserServiceApplication.java
    trying to fix register into eureka server

--- user-service/user-service/src/main/java/com/juanjo/user_service/UserServiceApplication.java
+++ user-service/user-service/src/main/java/com/juanjo/user_service/UserServiceApplication.java
@@ -3,11 +3,13 @@ package com.juanjo.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
