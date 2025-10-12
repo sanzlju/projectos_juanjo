70bd9c5fbc5ed96c80437878eb45fd14f792c712
sanzlju <jsanzleon@yahoo.com>
2025-10-12 21:46:38 +0200

    Update BikeServiceApplication.java
    trying to fix register into eureka server

--- bike-service/bike-service/src/main/java/com/juanjo/bike_service/BikeServiceApplication.java
+++ bike-service/bike-service/src/main/java/com/juanjo/bike_service/BikeServiceApplication.java
@@ -2,10 +2,12 @@ package com.juanjo.bike_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
 public class BikeServiceApplication {
 
 	public static void main(String[] args) {
